1fa2ba07ce73880a0bf3d0aa3d5666c218b408cc
Lee Seung Ho <lsho@charmeng.com>
2021-03-09 16:58:56 +0900

    동적 생성 기능 구현 중

--- skills/skills.js
+++ skills/skills.js
@@ -1,2 +1,26 @@
-document.getElementById('').innerHTML = `
-`;
+cir_progress = ;
+
+for (let index = 0; index < array.length; index++) {
+    cir_progress += `
+    <div class="col-md-3 col-sm-6 pt-5 item-div">
+                                    <img src="${a}" class="skill-img">
+                                    <div class="progress" data-percentage="${a}">
+                                        <span class="progress-left">
+                                            <span class="progress-bar" style="border-color: ${a};"></span>
+                                        </span>
+                                        <span class="progress-right">
+                                            <span class="progress-bar" style="border-color: ${a};"></span>
+                                        </span>
+                                        <div class="progress-value">
+                                            
+                                            <div>
+                                                <span>${a}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+    `
+    
+}
+
+document.getElementById('').innerHTML
