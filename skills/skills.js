a12894dfb05a180d9fe5739b20be6db02e3c6e60
Lee Seung Ho <lsho@charmeng.com>
2021-03-16 17:46:26 +0900

    동적 생성 및 title 추가

--- skills/skills.js
+++ skills/skills.js
@@ -1,29 +1,28 @@
-var array = [['skills-pictures/html.png','78','#f16529','HTML'],
-             ['skills-pictures/css.png','90','#2965f1','CSS'],
-             ['skills-pictures/javascript.png','85','#f7e018','JS'],
-             ['skills-pictures/sass.png','66','#ce679a','Sass']];
+var array = [['skills-pictures/html.png','77','#f16529','HTML'],
+             ['skills-pictures/css.png','80','#2965f1','CSS'],
+             ['skills-pictures/javascript.png','86','#f7e018','JS'],
+             ['skills-pictures/sass.png','65','#ce679a','Sass']];
+var cir_progress;
 
 for (let index = 0; index < array.length; index++) {
-    cir_progress += `
+    cir_progress = `
     <div class="col-md-3 col-sm-6 pt-5 item-div">
-                                    <img src="${a}" class="skill-img">
-                                    <div class="progress" data-percentage="${a}">
-                                        <span class="progress-left">
-                                            <span class="progress-bar" style="border-color: ${a};"></span>
-                                        </span>
-                                        <span class="progress-right">
-                                            <span class="progress-bar" style="border-color: ${a};"></span>
-                                        </span>
-                                        <div class="progress-value">
-                                            
-                                            <div>
-                                                <span>${a}</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+        <img src="${array[index][0]}" class="skill-img">
+        <div class="progress" data-percentage="${array[index][1]}">
+            <span class="progress-left">
+                <span class="progress-bar" style="border-color: ${array[index][2]};"></span>
+            </span>
+            <span class="progress-right">
+                <span class="progress-bar" style="border-color: ${array[index][2]};"></span>
+            </span>
+            <div class="progress-value">
+                <div>
+                    <span>${array[index][3]}</span>
+                </div>
+            </div>
+        </div>
+    </div>
     `
-    
+    document.getElementById('additemweb').innerHTML += cir_progress;
 }
 console.log(cir_progress);
-document.getElementById('additemweb').innerHTML(cir_progress);
